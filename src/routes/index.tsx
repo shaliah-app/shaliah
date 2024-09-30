4cd2e7bef235e5b4bf8e19810f2ed46d8c6fbd61
Patrick M <patrickmatias.silva@hotmail.com>
2024-09-30 19:27:47 +0000

    feat: add main controller grid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
-import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "./styles.css?inline";
 
 export default component$(() => {
-  return (
-    <>
-      hello, world!
-    </>
-  );
+  useStylesScoped$(styles);
+  return <main>hello, world!</main>;
 });
 
 export const head: DocumentHead = {
