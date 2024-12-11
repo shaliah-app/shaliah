b5b6d4803f7a363eae5e0711e3014aa9f708721e
Patrick M <patrickmatias.silva@hotmail.com>
2024-12-11 14:57:44 +0000

    feat(button): added `BooleanButton` variant

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -7,85 +7,93 @@ import {
 import { Icon } from "../icon/icon";
 import { css } from "~/utils/css";
 
-type ButtonProps = PropsOf<"button"> & {
+interface Variants {
   icon?: string;
   rounded?: boolean;
   wrapper?: boolean;
   size?: "lg" | "xl";
-};
+}
 
-export const Button = component$<ButtonProps>((props) => {
-  useStylesScoped$(css`
-    button {
-      --button-min-height: calc(2.5rem + var(--size, 0rem));
+const styles = css`
+  button,
+  label {
+    --button-min-height: calc(2.5rem + var(--size, 0rem));
 
-      width: fit-content;
-      min-height: var(--button-min-height);
-      height: fit-content;
-      border: none;
-      border-radius: 0.5rem;
-      padding: 0.5rem;
+    width: fit-content;
+    min-height: var(--button-min-height);
+    height: fit-content;
+    border: none;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
 
-      display: inline-flex;
-      align-items: center;
-      gap: 0.5rem;
-      flex-shrink: 0;
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+    flex-shrink: 0;
 
-      overflow: hidden;
-      cursor: pointer;
+    overflow: hidden;
+    cursor: pointer;
 
-      font-weight: 700;
-      color: var(--primary-color);
+    font-weight: 700;
+    color: var(--primary-color);
 
-      --shade-color: white;
-      --shade-percentage: 0%;
-      background-color: color-mix(
-        in srgb,
-        var(--bkg-color),
-        var(--shade-color) var(--shade-percentage)
-      );
-      transition: background-color 150ms ease-in-out;
+    --shade-color: white;
+    --shade-percentage: 0%;
+    background-color: color-mix(
+      in srgb,
+      var(--bkg-color),
+      var(--shade-color) var(--shade-percentage)
+    );
+    transition: background-color 150ms ease-in-out;
 
-      &:hover {
-        --shade-percentage: 10%;
-      }
+    &:hover {
+      --shade-percentage: 10%;
+    }
 
-      &:active {
-        --shade-percentage: 20%;
-      }
+    &:active {
+      --shade-percentage: 20%;
+    }
 
-      &:disabled {
-        --shade-percentage: 50%;
-        cursor: default;
+    &:disabled {
+      --shade-percentage: 50%;
+      cursor: default;
+      pointer-events: none;
+    }
+
+    /****************/
+    /*** Variants ***/
+    /****************/
+
+    &[rounded] {
+      justify-content: center;
+      aspect-ratio: 1;
+      border-radius: 100%;
+    }
+
+    &[wrapper] {
+      padding: 0;
+      border-radius: 0;
+      --shade-color: black;
+      --bkg-color: transparent;
+
+      > * {
         pointer-events: none;
-      }
-
-      /****************/
-      /*** Variants ***/
-      /****************/
-
-      &[rounded] {
-        justify-content: center;
-        aspect-ratio: 1;
-        border-radius: 100%;
-      }
-
-      &[wrapper] {
-        padding: 0;
-        border-radius: 0;
-        --shade-color: black;
-        --bkg-color: transparent;
-      }
-
-      &[size="lg"] {
-        --size: 1rem;
-      }
-
-      &[size="xl"] {
-        --size: 2rem;
+        z-index: -1;
       }
     }
-  `);
+
+    &[size="lg"] {
+      --size: 1rem;
+    }
+
+    &[size="xl"] {
+      --size: 2rem;
+    }
+  }
+`;
+
+export const Button = component$<PropsOf<"button"> & Variants>((props) => {
+  useStylesScoped$(styles);
 
   return (
     <button {...props}>
@@ -94,3 +102,22 @@ export const Button = component$<ButtonProps>((props) => {
     </button>
   );
 });
+
+export const BooleanButton = component$<PropsOf<"label"> & Variants>(
+  (props) => {
+    useStylesScoped$(styles);
+    useStylesScoped$(css`
+      input {
+        display: none;
+      }
+    `);
+
+    return (
+      <label {...props}>
+        <Slot />
+        {props.icon && <Icon>{props.icon}</Icon>}
+        <input type="checkbox" />
+      </label>
+    );
+  }
+);
