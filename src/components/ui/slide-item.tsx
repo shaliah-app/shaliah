b8b6227c12d52bc81a45f864f8932b7df077f40f
Patrick M <patrickmatias.silva@hotmail.com>
2024-12-13 01:07:15 +0000

    feat(slide-item): add transitions for states; adapted for swiperjs

--- src/components/ui/slide-item.tsx
+++ src/components/ui/slide-item.tsx
@@ -2,21 +2,17 @@ import { component$, Slot, useStylesScoped$ } from "@builder.io/qwik";
 import { css } from "~/utils/css";
 import Image from "@/slide-placeholder.jpg?jsx";
 
-
 export const SlideItem = component$(() => {
   useStylesScoped$(css`
-    figure {
-      height: 5rem;
-      border-radius: 2rem;
-
+    swiper-slide {
+      min-height: 5rem;
       display: flex;
       justify-content: space-between;
       align-items: center;
 
-      background-color: var(--bkg-color);
       overflow: hidden;
 
-      > figcaption {
+      > span {
         width: 100%;
         height: 1.1rem;
         margin-bottom: -0.1rem; /* magic margin needed to remove vertical overflow made by overflow-x property */
@@ -28,11 +24,11 @@ export const SlideItem = component$(() => {
         overflow-x: hidden;
 
         &:empty::before {
-          content: "default-file.png"
+          content: "default-file.png";
         }
       }
 
-      & #image-wrapper {
+      & .wrapper {
         width: 25%;
 
         display: grid;
@@ -46,14 +42,57 @@ export const SlideItem = component$(() => {
           display: block;
         }
       }
+
+      /* ******************************** */
+      /* Start - Colors transition styles */
+      /* ******************************** */
+
+      transition-property: background-color, color;
+      transition-duration: 150ms;
+      transition-timing-function: ease-in-out;
+
+      --shade-color: rgb(0, 0, 0, 0);
+      --hover-color: rgb(255, 255, 255, 0);
+      --selected-color: var(--primary-color);
+      --selected-percentage: 0%;
+      --slide-bkg-color: color-mix(
+        in srgb,
+        var(--shade-color),
+        var(--selected-color) var(--selected-percentage)
+      );
+
+      background-color: color-mix(
+        in srgb,
+        var(--hover-color),
+        var(--slide-bkg-color)
+      );
+
+      &:hover {
+        --hover-color: rgb(255, 255, 255, 0.1);
+      }
+
+      &:nth-child(even) {
+        --shade-color: rgb(0, 0, 0, 0.1);
+      }
+
+      &.swiper-slide-active {
+        --selected-percentage: 100%;
+        color: color-mix(in srgb, var(--bkg-color) 100%,rgb(0, 0, 0) 50%);
+      }
+      
+      /* ****************************** */
+      /* End - Colors transition styles */
+      /* ****************************** */
     }
   `);
   return (
-    <figure>
-      <figcaption><Slot /></figcaption>
-      <div id="image-wrapper">
-        <Image />
-      </div>
-    </figure>
+    <swiper-slide>
+        <span>
+          <Slot />
+        </span>
+        <div class="wrapper">
+          <Image />
+        </div>
+    </swiper-slide>
   );
 });
