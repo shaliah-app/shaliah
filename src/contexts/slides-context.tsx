22901c16e735497ba587f4906ad1c592fe9adc1f
Patrick M <patrickmatias.silva@hotmail.com>
2024-12-16 23:21:06 -0300

    refactor: place slides references in separated `.json` file

--- src/contexts/slides-context.tsx
+++ src/contexts/slides-context.tsx
@@ -8,6 +8,8 @@ import {
   useStore,
 } from "@builder.io/qwik";
 
+import array from "@/slides.json";
+
 interface Slide {
   id: number;
   file_name: string;
@@ -31,33 +33,7 @@ export const SlidesContextProvider = component$(() => {
     set active(value) {
       this._active = value;
     },
-    array: [
-      {
-        id: 1,
-        file_name: "peak.jpg",
-        preview: "slide-placeholder-1.jpg",
-      },
-      {
-        id: 2,
-        file_name: "from-the-sky.png",
-        preview: "slide-placeholder-2.jpg",
-      },
-      {
-        id: 3,
-        file_name: "lavender fields by chopin.jpg",
-        preview: "slide-placeholder-3.jpg",
-      },
-      {
-        id: 4,
-        file_name: "SunsetBest.jpg",
-        preview: "slide-placeholder-4.jpg",
-      },
-      {
-        id: 5,
-        file_name: "SpookyFrozenMountains.png",
-        preview: "slide-placeholder-5.jpg",
-      },
-    ],
+    array,
   }));
 
   useOnWindow(
