a68b47087b7765eefef76bb6c6772caebd871033
Patrick M <patrickmatias.silva@hotmail.com>
2024-12-18 16:31:52 -0300

    feat(slide-item): add side menu

--- src/components/ui/slide-item.tsx
+++ src/components/ui/slide-item.tsx
@@ -1,44 +1,17 @@
 import { component$, Slot, useStylesScoped$ } from "@builder.io/qwik";
 import { css } from "~/utils/css";
+import { Button } from "./button";
 
 export const SlideItem = component$(() => {
   useStylesScoped$(css`
-    swiper-slide {
+    .slide-root {
       min-height: 5rem;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
 
-      overflow: hidden;
+      > swiper-container {
+        height: 100%;
 
-      > span {
-        width: 100%;
-        height: 1.1rem;
-        margin-bottom: -0.1rem; /* magic margin needed to remove vertical overflow made by overflow-x property */
-        margin-inline: 1rem;
-
-        font-weight: 500;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        overflow-x: hidden;
-
-        &:empty::before {
-          content: "default-file.png";
-        }
-      }
-
-      & .preview-wrapper {
-        width: 25%;
-
-        display: grid;
-        place-content: center;
-        flex-shrink: 0;
-        overflow: hidden;
-
-        > img {
-          width: 100%;
-          height: auto;
-          display: block;
+        & swiper-slide {
+          width: auto;
         }
       }
 
@@ -83,15 +56,69 @@ export const SlideItem = component$(() => {
       /* End - Colors transition styles */
       /* ****************************** */
     }
+
+    .slide-content {
+      max-width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      overflow: hidden;
+
+      > span {
+        width: 100%;
+        height: 1.1rem;
+        margin-bottom: -0.1rem; /* magic margin needed to remove vertical overflow made by overflow-x property */
+        margin-inline: 1rem;
+
+        font-weight: 500;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow-x: hidden;
+
+        &:empty::before {
+          content: "default-file.png";
+        }
+      }
+
+      & .preview-wrapper {
+        width: 25%;
+
+        display: grid;
+        place-content: center;
+        flex-shrink: 0;
+        overflow: hidden;
+
+        > img {
+          width: 100%;
+          height: auto;
+          display: block;
+        }
+      }
+    }
+
+    aside {
+      display: flex;
+      padding: 1rem;
+      /* background-color: var(--primary-color); */
+    }
   `);
   return (
-    <swiper-slide>
-      <span>
-        <Slot />
-      </span>
-      <div class="preview-wrapper">
-        <Slot name="preview" />
-      </div>
+    <swiper-slide class="slide-root">
+      <swiper-container slides-per-view="auto" touch-release-on-edges="true">
+        <swiper-slide class="slide-content">
+          <span>
+            <Slot />
+          </span>
+          <div class="preview-wrapper" role="none">
+            <Slot name="preview" />
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <aside role="toolbar" aria-label="Slide controls">
+            <Button class="red size-lg" tabIndex={-1} icon="delete" />
+          </aside>
+        </swiper-slide>
+      </swiper-container>
     </swiper-slide>
   );
 });
