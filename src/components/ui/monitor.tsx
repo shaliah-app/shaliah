68b37f3179d51acdd1464dd29394fc0cfe627c09
Patrick M <patrickmatias.silva@hotmail.com>
2024-12-24 19:18:04 -0300

    feat(monitor): change slides moving left/right

--- src/components/ui/monitor.tsx
+++ src/components/ui/monitor.tsx
@@ -1,52 +1,91 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  $,
+  component$,
+  useContext,
+  useId,
+  useOnDocument,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 import { css } from "~/utils/css";
 
-import { BooleanButton, Button } from "./button";
+import { BooleanButton } from "./button";
+import { SlidesContextId } from "~/contexts/slides-context";
+import { Image } from "@unpic/qwik";
+import type { Swiper } from "swiper/types";
+import { lettersAndNumbers } from "~/utils/letters-and-numbers-substring";
 
 export const Monitor = component$(() => {
   useStylesScoped$(css`
     div {
-      width: 100%;
+      max-width: 80%;
+      height: 70%;
       margin: 0 auto;
+      box-shadow: 0px 10px 30px #00000025;
 
-      display: grid;
-      grid-auto-flow: column;
-      place-content: center;
-      align-items: center;
+      position: relative;
 
-      .wrapper {
-        width: auto;
-        margin-inline: 1.5rem;
-        box-shadow: 0px 10px 30px #00000025;
+      & iframe {
         /* TODO: should be according monitor screen size, and not fixed like this */
-        aspect-ratio: 16 / 10; 
-
-        > iframe {
-          width: 100%;
-          height: 100%;
-        }
+        aspect-ratio: 16 / 10;
       }
 
-      button {
-        display: none;
-        grid-row: 1;
+      & iframe,
+      label {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    swiper-container {
+      width: 100%;
+      height: 100%;
+
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      img {
+        transition-property: opacity;
+        transition-duration: 500ms;
+        transition-timing-function: ease-in;
       }
 
-      &:has(:checked) button {
-        display: inline-flex;
+      .swiper-slide-active img {
+        opacity: 0;
       }
     }
   `);
 
+  const slides = useContext(SlidesContextId);
+
+  const id = lettersAndNumbers(useId());
+
+  useOnDocument(
+    `${id}slidechange`,
+    $((e) => {
+      const { swiper } = e.target as EventTarget & { swiper: Swiper };
+      const index = swiper.activeIndex;
+      slides.active = slides.array[index];
+    })
+  );
+
   return (
-    <div role="toolbar">
-      <Button class="rounded size-lg" icon="arrow_back" />
-
-      <BooleanButton class="wrapper">
-        <iframe src="/present" loading="lazy"></iframe>
-      </BooleanButton>
-
-      <Button class="rounded size-lg" icon="arrow_forward" />
+    <div>
+      <iframe src="/present" loading="lazy"></iframe>
+      <swiper-container
+        centered-slides="true"
+        grab-cursor="true"
+        events-prefix={id}
+        touch-release-on-edges="true"
+      >
+        {slides.array.map((s) => (
+          <swiper-slide key={s.id}>
+            <BooleanButton class="wrapper">
+              <Image src={s.preview} />
+            </BooleanButton>
+          </swiper-slide>
+        ))}
+      </swiper-container>
     </div>
   );
 });
