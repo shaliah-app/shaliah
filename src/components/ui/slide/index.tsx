3840f7912a064307119cb621cab7eb94b36601a4
Patrick M <patrickmatias.silva@hotmail.com>
2025-02-19 10:36:42 -0300

    chore(slide): remove user selection

--- src/components/ui/slide/index.tsx
+++ src/components/ui/slide/index.tsx
@@ -19,6 +19,7 @@ export const Slide = component$<{ slide: SlideEntity }>((props) => {
       justify-content: space-between;
       align-items: center;
       overflow: hidden;
+      user-select: none;
 
       > span {
         width: 100%;
