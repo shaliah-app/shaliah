8b052c5ad1cd2c4a8b3b12dda62d2e51cbc254c9
Patrick M <patrickmatias.silva@hotmail.com>
2025-05-02 12:39:50 -0300

    fix: correct and clearer `usePrevious` hook

--- src/hooks/usePrevious.tsx
+++ src/hooks/usePrevious.tsx
@@ -1,17 +1,17 @@
-// Currently not used.
 // Useful in a context where you need the old value
 // of a track() update.
 
-import { useSignal, useTask$, type Signal } from "@qwik.dev/core";
+import { useStore, useTask$ } from "@qwik.dev/core";
 
-export function usePrevious<T>(value: T): Signal<T | undefined> {
-  const previousValue = useSignal<T | undefined>(undefined);
+export function usePrevious<STATE extends object>(value: STATE): STATE {
+  const actualValue = useStore<STATE>({ ...value });
+  const previousValue = useStore<STATE>({ ...value });
 
   useTask$(({ track }) => {
-    track(() => value);
-    previousValue.value = value;
+    const newValue = track(value);
+    Object.assign(previousValue, actualValue);
+    Object.assign(actualValue, newValue);
   });
-  
+
   return previousValue;
 }
-
