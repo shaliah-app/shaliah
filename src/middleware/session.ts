0b0e718b5953f8490834f84fd8306f1d14f3285b
Patrick M <patrickmatias.silva@hotmail.com>
2025-05-02 12:38:37 -0300

    feat: add server `SessionService` with factory

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,20 +1,10 @@
 import type { RequestHandler } from "@qwik.dev/router";
-import { v4 as uuidv4 } from "uuid";
+import { SessionService } from "~/services/SessionService";
 
 export const sessionMiddleware: RequestHandler = async ({ cookie, next }) => {
-  const sessionId = cookie.get("session-id");
+  const sessionId = cookie.get(SessionService.SESSION_COOKIE_NAME);
 
-  if (!sessionId) {
-    const id = uuidv4();
-
-    cookie.set("session-id", id, {
-      path: "/",
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24 * 365, // 1 year
-      sameSite: "strict",
-    });
-  }
+  if (!sessionId) SessionService.createSessionId(cookie);
 
   return await next();
 };
